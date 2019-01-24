b1d1755d51cc0fad494b68fbf20b8a5ada3d7d07
Gerry Agbobada <gagbobada+git@gmail.com>
2019-01-24 18:05:44 +0100

    Change colorscheme

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#CEDBE5", "#0C0C14" },
+	[SchemeSel] = { "#E5F4FF", "#247AB7" },
+	[SchemeOut] = { "#0D0D16", "#39D7E5" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
